886cd82802b141710b95332133fded115b87be1e
Tim Boode <tboode@everyangle.org>
2016-01-05 21:18:31 +0100

    initial commit

---
+++ ubagame/ubagame/ubagame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ubagame
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Thread.Sleep(50000);
+        }
+    }
+}
